2dc3eb720ec9434fdd808f1fa0e963209f937377
Ravindra Kachariya <ravindrakachariya@gmail.com>
2024-07-19 16:26:11 +0530

    Update Ans-6.c

--- CH - 5 - Looping/5 - Looping With Patterns/5.1 - Without Space/Ans-6.c
+++ CH - 5 - Looping/5 - Looping With Patterns/5.1 - Without Space/Ans-6.c
@@ -5,12 +5,12 @@ int main()
 	int i,j;
 	
 	for(i=5;i>=1;i--)
-    {
+    	{
 		for(j=1;j<=i;j++)
-        {
-			(j%2==0)
-				?printf("0 ")
-				:printf("1 ");
+      		{
+		(j%2==0)
+			?printf("0 ")
+			:printf("1 ");
 		}
 		printf("\n");
 	}
