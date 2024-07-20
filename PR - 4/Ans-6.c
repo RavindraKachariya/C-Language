a9e33cd74b516d2fac341f2164efa060d3ed8a43
Ravindra Kachariya <ravindrakachariya@gmail.com>
2024-07-20 11:40:22 +0530

    Add | PR - 4

--- PR - 4/Ans-6.c
+++ PR - 4/Ans-6.c
@@ -5,17 +5,17 @@ main()
 	int i,j,s;
 
 	for(i=5;i>=1;i--)
-    {
+   	 {
 		for(s=1;s<=i-1;s++)
-        {
-			printf("  ");
-		}
+         	{
+		printf("  ");
+	 	}
 		for(j=i;j<=5;j++)
-        {
+        	{
 			printf("%d ",j);
 		}
 		for(j=4;j>=i;j--)
-        {
+        	{
 			printf("%d ",j);
 		}
 		printf("\n");
