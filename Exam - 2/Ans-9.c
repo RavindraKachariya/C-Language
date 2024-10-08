43dcd870d81d23c9df4a6ba7c8cf7782fb703691
Ravindra Kachariya <ravindrakachariya@gmail.com>
2024-10-08 22:44:39 +0530

    Update Ans-9.c

--- Exam - 2/Ans-9.c
+++ Exam - 2/Ans-9.c
@@ -1,50 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int a;
+    float percentage;
+    printf("Enter Percentage: ");
+    scanf("%f", &percentage);
 
-	printf("Enter Your Persentage : ");
-	scanf("%d",&a);
+    if (percentage >= 90)
+    {
+        printf("Grade: A");
+    }
+    else if (percentage >= 80)
+    {
+        printf("Grade: B");
+    }
+    else if (percentage >= 70)
+    {
+        printf("Grade: C");
+    }
+    else if (percentage >= 60)
+    {
+        printf("Grade: D");
+    }
+    else if (percentage >= 50)
+    {
+        printf("Grade: E");
+    }
+    else
+    {
+        printf("Grade: Failed");
+    }
 
-	if(a>90 && a<=100)
-	{
-		printf("Your Grade Is = A1");
-	}
-
-	else if(a>80 && a<=90)
-	{
-		printf("Your Grade Is = A2 ");
-	}
-
-	else if(a>70 && a<=80)
-	{
-		printf("Your Grade Is = B1 ");
-	}
-
-	else if(a>60 && a<=70)
-	{
-		printf("Your Grade Is = B2 ");
-	}
-
-	else if(a>50 && a<=60)
-	{
-		printf("Your Grade Is = C1 ");
-	}
-
-	else if(a>40 && a<=50)
-	{
-		printf("Your Grade Is = C2 ");
-	}
-
-	else if(a>=33 && a<=40)
-	{
-		printf("Your Grade Is = D1");
-	}
-
-	else
-	{
-		printf("Need Improvement");
-	}
-    
+    return 0;
 }
